a89fcb4c6e2f50946996aca766129cc077824127
owner <struwwel4peter@gmail.com>
2023-08-20 13:36:27 +0200

    First JPA Entities

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,61 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+
+	private String isbn;
+
+	private Set<Author> author;
+
+	public Book()
+	{
+	}
+
+	public Book(String title, String isbn, Set<Author> author)
+	{
+		this.title = title;
+		this.isbn = isbn;
+		this.author = author;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public Set<Author> getAuthor()
+	{
+		return author;
+	}
+
+	public void setAuthor(Set<Author> author)
+	{
+		this.author = author;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public String getIsbn()
+	{
+		return isbn;
+	}
+
+	public void setIsbn(String isbn)
+	{
+		this.isbn = isbn;
+	}
+}
